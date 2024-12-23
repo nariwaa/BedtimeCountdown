12ef47ddde76f6eb97271732dfe95d88d8ad91df
nariwaa <lucasval3n2c@gmail.com>
2024-12-23 17:53:18 +0100

    moved everything to the app dir

---
+++ app/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      theme: ThemeData(
+        useMaterial3: true,
+        // Use dynamic color if the platform supports it, otherwise fall back to a default color
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow, // Default seed color, can be any color
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow, // Default seed color for dark mode, can be any color
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Alarm Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Settings Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bedtime Countdown'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'Alarm',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ app/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      file
+      flutter319
+      android-studio
+      android-tools
+      sdkmanager
+      jdk8
+      google-chrome
+    ];
+
+  shellHook = ''
+#echo -e "\033[33mCreating the flutter venv\033[0m"
+
+echo -e "\033[33m\nDone!\033[0m"
+  '';
+}
